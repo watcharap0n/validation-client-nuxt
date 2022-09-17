860851657b7692f423c0f53c98c8d62d0e857b33
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2022-09-17 10:34:48 +0700

    update page error

--- client/app/layouts/error.vue
+++ client/app/layouts/error.vue
@@ -1,31 +1,37 @@
 <template>
   <v-container>
-    <v-row align="center" justify="center">
-      <div class="p-10">
-        <v-card>
-          <v-card-title>Page Not Found</v-card-title>
-          <v-card-text>
-            Looks like you've followed a broken link or entered <br>
-            a URL that doesn't exits on site
-          </v-card-text>
-          <v-card-actions>
-            <v-btn text
-                   color="info"
-                   to="/validate"
-            >
-              Back to our site
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </div>
-    </v-row>
+    <div class="container">
+      <v-row v-if="error.statusCode === 404" align="center" justify="center">
+        <div class="p-10">
+          <v-card>
+            <v-card-title>Page Not Found</v-card-title>
+            <v-card-text>
+              Looks like you've followed a broken link or entered <br>
+              a URL that doesn't exits on site
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text
+                     color="info"
+                     to="/validate"
+              >
+                Back to our site
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+      </v-row>
+
+      <v-row v-else>
+        <h1>An error occurred</h1>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
 <script>
 export default {
   props: ['error'],
-  layout: 'empty'
+  layout: 'empty',
 }
 </script>
 
